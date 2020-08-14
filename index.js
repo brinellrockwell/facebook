ed46570fd2db187eaed66f4a398478005ddeca48
brinellrockwell <69689339+brinellrockwell@users.noreply.github.com>
2020-08-14 18:40:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 //index.js  
 function sendEmail() {
 	Email.send({
-    SecureToken: "1067F2320BE5C31E21188BB08439F7A2E163",
-	To : 'brinell.rockwell@gmail.com',
-	From : "brinell.rockwell@gmail.com",
-	Subject : "Elba2f lbesha",
-	Body : "lebes",
+		SecureToken: "1067F2320BE5C31E21188BB08439F7A2E163",
+		Host: "smtp.elasticemail.com",
+		Username : "brinell.rockwell@gmail.com",
+		Password : "l0gmeinn0w",
+		To : 'karimshalapy@gmail.com',
+		From : "brinell.rockwell@gmail.com",
+		Subject : "Elba2f lbesha",
+		Body : "lebes",
 	}).then(
 		message => alert("mail sent successfully")
     );
